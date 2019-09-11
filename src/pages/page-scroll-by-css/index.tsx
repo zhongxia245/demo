14725ce3636a6cf64471683d568cbde5946194b3
zhongxia <huangxiaodong@licaigc.com>
2019-09-11 22:33:16 +0800

    feature:使用 css隐藏微信下拉展示的网址

---
+++ src/pages/page-scroll-by-css/index.tsx
@@ -0,0 +1,51 @@
+import styles from './index.less';
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = function() {
+  return (
+    <div className={styles.normal}>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+      <h1>Page forbid-page-scroll</h1>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById('root'));
