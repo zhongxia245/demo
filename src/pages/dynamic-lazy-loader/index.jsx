f93309734d1960dd842e4effaaa3ddf7f3a20053
zhongxia <zhongxia245@sina.com>
2020-01-04 19:46:19 +0800

    fix:尝试 react 组件懒加载,webpack 打包还会单独打包吗

--- src/pages/dynamic-lazy-loader/index.jsx
+++ src/pages/dynamic-lazy-loader/index.jsx
@@ -1,35 +1,45 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useEffect, useState } from 'react';
 import { render } from 'react-dom';
 import './index.less';
 
-const config = [
+const DEFAULT_CONFIG = [
   { name: '组件 A', props: { name: 'zhongxia' }, componentName: 'A.js' },
   { name: '组件 B', props: { age: 18 }, componentName: 'D.js' },
   { name: '组件 C', props: { score: 99 }, componentName: 'C.js' },
 ];
 
 function App() {
+  const [config, setConfig] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setConfig(DEFAULT_CONFIG);
+    }, 2000);
+  }, []);
+
   return (
     <div className="normal">
       <h1>动态按需加载模块</h1>
-      {config.map((item, index) => {
-        let Comp = lazy(() =>
-          import(`./component/${item.componentName}`).catch(err => {
-            return {
-              default: () => <div>{String(err)}</div>,
-            };
-          }),
-        );
+      {config.length > 0
+        ? config.map((item, index) => {
+            let Comp = lazy(() =>
+              import(`./component/${item.componentName}`).catch(err => {
+                return {
+                  default: () => <div>{String(err)}</div>,
+                };
+              }),
+            );
 
-        return (
-          <div className="module" key={index}>
-            {item.name}
-            <Suspense fallback={<div>组件加载中...</div>} key={index}>
-              <Comp />
-            </Suspense>
-          </div>
-        );
-      })}
+            return (
+              <div className="module" key={index}>
+                {item.name}
+                <Suspense fallback={<div>组件加载中...</div>} key={index}>
+                  <Comp />
+                </Suspense>
+              </div>
+            );
+          })
+        : '数据加载中...'}
     </div>
   );
 }
